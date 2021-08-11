955d2ff2db4bb6f60eb35b398c6c3aaf559cd886
Huy Duong <huy.duong.ext@personio.de>
2021-08-11 11:34:30 +0700

    Support bigger and smaller

--- lua/nvim-tree-custom.lua
+++ lua/nvim-tree-custom.lua
@@ -1,101 +1,109 @@
-local lib = require "nvim-tree.lib"
-local pops = require "nvim-tree.populate"
-local state = require "nvim-tree.state"
-local utils = require "nvim-tree.utils"
-local view = require "nvim-tree.view"
-local fs = require "nvim-tree.fs"
-local fs_custom = require "nvim-tree.fs-custom"
+local lib = require("nvim-tree.lib")
+local pops = require("nvim-tree.populate")
+local state = require("nvim-tree.state")
+local utils = require("nvim-tree.utils")
+local view = require("nvim-tree.view")
+local fs = require("nvim-tree.fs")
+local fs_custom = require("nvim-tree.fs-custom")
 
 local M = {}
 
 local function focus_file(file)
-  local _, i = utils.find_node(lib.Tree.entries, function(node)
-    return node.absolute_path == file
-  end)
-  view.set_cursor { i + 1, 1 }
+	local _, i = utils.find_node(lib.Tree.entries, function(node)
+		return node.absolute_path == file
+	end)
+	view.set_cursor({ i + 1, 1 })
 end
 
 M.keypress_funcs = {
-  toggle_selection = function(node)
-    state.toggle_selection(node)
-    lib.redraw()
-  end,
-  clear_selections = function()
-    state.clear_selections()
-    lib.redraw()
-  end,
-  delete = function(node)
-    local nodes = state.get_selected_nodes()
+	toggle_selection = function(node)
+		state.toggle_selection(node)
+		lib.redraw()
+	end,
+	clear_selections = function()
+		state.clear_selections()
+		lib.redraw()
+	end,
+	delete = function(node)
+		local nodes = state.get_selected_nodes()
 
-    if next(nodes) == nil then
-      -- If select nothing then delete the current node
-      fs.remove(node)
-      return
-    end
+		if next(nodes) == nil then
+			-- If select nothing then delete the current node
+			fs.remove(node)
+			return
+		end
 
-    print "Remove selected nodes ? y/n"
-    local ans = utils.get_user_input_char()
-    utils.clear_prompt()
+		print("Remove selected nodes ? y/n")
+		local ans = utils.get_user_input_char()
+		utils.clear_prompt()
 
-    if ans:match "^y" then
-      fs_custom.delete(nodes)
-      lib.refresh_tree()
-      state.clear_selections()
-    end
-  end,
-  copy = function(node)
-    local nodes = state.get_selected_nodes()
+		if ans:match("^y") then
+			fs_custom.delete(nodes)
+			lib.refresh_tree()
+			state.clear_selections()
+		end
+	end,
+	copy = function(node)
+		local nodes = state.get_selected_nodes()
 
-    if next(nodes) == nil then
-      -- If select nothing then delete the current node
-      print "Nothing to copy"
-      return
-    end
+		if next(nodes) == nil then
+			-- If select nothing then delete the current node
+			print("Nothing to copy")
+			return
+		end
 
-    print "Copy selected nodes ? y/n"
-    local ans = utils.get_user_input_char()
-    utils.clear_prompt()
+		print("Copy selected nodes ? y/n")
+		local ans = utils.get_user_input_char()
+		utils.clear_prompt()
 
-    if ans:match "^y" then
-      local dest_paths = fs_custom.copy(node, nodes)
-      lib.refresh_tree()
-      state.clear_selections()
-      vim.notify(dest_paths[1], vim.log.levels.ERROR)
+		if ans:match("^y") then
+			local dest_paths = fs_custom.copy(node, nodes)
+			lib.refresh_tree()
+			state.clear_selections()
+			vim.notify(dest_paths[1], vim.log.levels.ERROR)
 
-      if #dest_paths == 1 then
-        focus_file(dest_paths[0] or dest_paths[1])
-      end
-    end
-  end,
-  cut = function(node)
-    local nodes = state.get_selected_nodes()
+			if #dest_paths == 1 then
+				focus_file(dest_paths[0] or dest_paths[1])
+			end
+		end
+	end,
+	cut = function(node)
+		local nodes = state.get_selected_nodes()
 
-    if next(nodes) == nil then
-      -- If select nothing then delete the current node
-      print "Nothing to move"
-      return
-    end
+		if next(nodes) == nil then
+			-- If select nothing then delete the current node
+			print("Nothing to move")
+			return
+		end
 
-    print "Move selected nodes ? y/n"
-    local ans = utils.get_user_input_char()
-    utils.clear_prompt()
+		print("Move selected nodes ? y/n")
+		local ans = utils.get_user_input_char()
+		utils.clear_prompt()
 
-    if ans:match "^y" then
-      local dest_paths = fs_custom.cut(node, nodes)
-      lib.refresh_tree()
-      state.clear_selections()
+		if ans:match("^y") then
+			local dest_paths = fs_custom.cut(node, nodes)
+			lib.refresh_tree()
+			state.clear_selections()
 
-      if #dest_paths == 1 then
-        focus_file(dest_paths[0] or dest_paths[1])
-      end
-    end
-  end,
-  toggle_hidden = function()
-    pops.show_ignored = not pops.show_ignored
-    pops.show_dotfiles = not pops.show_dotfiles
+			if #dest_paths == 1 then
+				focus_file(dest_paths[0] or dest_paths[1])
+			end
+		end
+	end,
+	toggle_hidden = function()
+		pops.show_ignored = not pops.show_ignored
+		pops.show_dotfiles = not pops.show_dotfiles
 
-    lib.refresh_tree()
-  end,
+		lib.refresh_tree()
+	end,
+	bigger = function()
+		local winnr = view.get_winnr()
+		vim.api.nvim_win_set_width(winnr, vim.api.nvim_win_get_width(winnr) + 2)
+	end,
+	smaller = function()
+		local winnr = view.get_winnr()
+		vim.api.nvim_win_set_width(winnr, vim.api.nvim_win_get_width(winnr) - 2)
+	end,
 }
 
 return M
--- lua/nvim-tree/fs-custom.lua
+++ lua/nvim-tree/fs-custom.lua
@@ -1,196 +1,196 @@
 local api = vim.api
 local luv = vim.loop
 
-local utils = require "nvim-tree.utils"
-local events = require "nvim-tree.events"
-local lib = require "nvim-tree.lib"
+local utils = require("nvim-tree.utils")
+local events = require("nvim-tree.events")
+local lib = require("nvim-tree.lib")
 local M = {}
 
 local function clear_buffer(absolute_path)
-  for _, buf in pairs(api.nvim_list_bufs()) do
-    if vim.fn.bufloaded(buf) == 1 and api.nvim_buf_get_name(buf) == absolute_path then
-      api.nvim_command(":bd! " .. buf)
-    end
-  end
+	for _, buf in pairs(api.nvim_list_bufs()) do
+		if vim.fn.bufloaded(buf) == 1 and api.nvim_buf_get_name(buf) == absolute_path then
+			api.nvim_command(":bd! " .. buf)
+		end
+	end
 end
 
 local function remove_dir(cwd)
-  local handle = luv.fs_scandir(cwd)
-  if type(handle) == "string" then
-    return api.nvim_err_writeln(handle)
-  end
+	local handle = luv.fs_scandir(cwd)
+	if type(handle) == "string" then
+		return api.nvim_err_writeln(handle)
+	end
 
-  while true do
-    local name, t = luv.fs_scandir_next(handle)
-    if not name then
-      break
-    end
+	while true do
+		local name, t = luv.fs_scandir_next(handle)
+		if not name then
+			break
+		end
 
-    local new_cwd = utils.path_join { cwd, name }
-    if t == "directory" then
-      local success = remove_dir(new_cwd)
-      if not success then
-        return false
-      end
-    else
-      local success = luv.fs_unlink(new_cwd)
-      if not success then
-        return false
-      end
-      clear_buffer(new_cwd)
-    end
-  end
+		local new_cwd = utils.path_join({ cwd, name })
+		if t == "directory" then
+			local success = remove_dir(new_cwd)
+			if not success then
+				return false
+			end
+		else
+			local success = luv.fs_unlink(new_cwd)
+			if not success then
+				return false
+			end
+			clear_buffer(new_cwd)
+		end
+	end
 
-  return luv.fs_rmdir(cwd)
+	return luv.fs_rmdir(cwd)
 end
 
 local function remove(node)
-  if node.entries ~= nil then
-    local success = remove_dir(node.absolute_path)
-    if not success then
-      return api.nvim_err_writeln("Could not remove " .. node.name)
-    end
-    events._dispatch_folder_removed(node.absolute_path)
-  else
-    local success = luv.fs_unlink(node.absolute_path)
-    if not success then
-      return api.nvim_err_writeln("Could not remove " .. node.name)
-    end
-    events._dispatch_file_removed(node.absolute_path)
-    clear_buffer(node.absolute_path)
-  end
+	if node.entries ~= nil then
+		local success = remove_dir(node.absolute_path)
+		if not success then
+			return api.nvim_err_writeln("Could not remove " .. node.name)
+		end
+		events._dispatch_folder_removed(node.absolute_path)
+	else
+		local success = luv.fs_unlink(node.absolute_path)
+		if not success then
+			return api.nvim_err_writeln("Could not remove " .. node.name)
+		end
+		events._dispatch_file_removed(node.absolute_path)
+		clear_buffer(node.absolute_path)
+	end
 end
 
 local function rename_loaded_buffers(old_name, new_name)
-  for _, buf in pairs(api.nvim_list_bufs()) do
-    if api.nvim_buf_is_loaded(buf) then
-      if api.nvim_buf_get_name(buf) == old_name then
-        api.nvim_buf_set_name(buf, new_name)
-        -- to avoid the 'overwrite existing file' error message on write
-        vim.api.nvim_buf_call(buf, function()
-          vim.cmd "silent! w!"
-        end)
-      end
-    end
-  end
+	for _, buf in pairs(api.nvim_list_bufs()) do
+		if api.nvim_buf_is_loaded(buf) then
+			if api.nvim_buf_get_name(buf) == old_name then
+				api.nvim_buf_set_name(buf, new_name)
+				-- to avoid the 'overwrite existing file' error message on write
+				vim.api.nvim_buf_call(buf, function()
+					vim.cmd("silent! w!")
+				end)
+			end
+		end
+	end
 end
 
 local function do_cut(source, destination)
-  local success = luv.fs_rename(source, destination)
-  if not success then
-    return success
-  end
-  rename_loaded_buffers(source, destination)
-  return true
+	local success = luv.fs_rename(source, destination)
+	if not success then
+		return success
+	end
+	rename_loaded_buffers(source, destination)
+	return true
 end
 
 local function do_copy(source, destination)
-  local source_stats = luv.fs_stat(source)
+	local source_stats = luv.fs_stat(source)
 
-  if source_stats and source_stats.type == "file" then
-    return luv.fs_copyfile(source, destination)
-  end
+	if source_stats and source_stats.type == "file" then
+		return luv.fs_copyfile(source, destination)
+	end
 
-  local handle = luv.fs_scandir(source)
+	local handle = luv.fs_scandir(source)
 
-  if type(handle) == "string" then
-    return false, handle
-  end
+	if type(handle) == "string" then
+		return false, handle
+	end
 
-  luv.fs_mkdir(destination, source_stats.mode)
+	luv.fs_mkdir(destination, source_stats.mode)
 
-  while true do
-    local name, _ = luv.fs_scandir_next(handle)
-    if not name then
-      break
-    end
+	while true do
+		local name, _ = luv.fs_scandir_next(handle)
+		if not name then
+			break
+		end
 
-    local new_name = utils.path_join { source, name }
-    local new_destination = utils.path_join { destination, name }
-    local success, msg = do_copy(new_name, new_destination)
-    if not success then
-      return success, msg
-    end
-  end
+		local new_name = utils.path_join({ source, name })
+		local new_destination = utils.path_join({ destination, name })
+		local success, msg = do_copy(new_name, new_destination)
+		if not success then
+			return success, msg
+		end
+	end
 
-  return true
+	return true
 end
 
 local function do_single_paste(source, dest, action_type, action_fn)
-  local dest_stats = luv.fs_stat(dest)
-  local should_process = true
-  local should_rename = false
+	local dest_stats = luv.fs_stat(dest)
+	local should_process = true
+	local should_rename = false
 
-  if dest_stats then
-    utils.clear_prompt()
-    print(dest .. " already exists. Overwrite? y/n/r(ename)")
-    local ans = utils.get_user_input_char()
-    utils.clear_prompt()
-    should_process = ans:match "^y"
-    should_rename = ans:match "^r"
-  end
+	if dest_stats then
+		utils.clear_prompt()
+		print(dest .. " already exists. Overwrite? y/n/r(ename)")
+		local ans = utils.get_user_input_char()
+		utils.clear_prompt()
+		should_process = ans:match("^y")
+		should_rename = ans:match("^r")
+	end
 
-  if should_rename then
-    local new_dest = vim.fn.input("New name: ", dest)
-    return do_single_paste(source, new_dest, action_type, action_fn)
-  end
+	if should_rename then
+		local new_dest = vim.fn.input("New name: ", dest)
+		return do_single_paste(source, new_dest, action_type, action_fn)
+	end
 
-  if should_process then
-    local success, errmsg = action_fn(source, dest)
-    if not success then
-      api.nvim_err_writeln("Could not " .. action_type .. " " .. source .. " - " .. errmsg)
-      return nil
-    else
-      return dest
-    end
-  end
+	if should_process then
+		local success, errmsg = action_fn(source, dest)
+		if not success then
+			api.nvim_err_writeln("Could not " .. action_type .. " " .. source .. " - " .. errmsg)
+			return nil
+		else
+			return dest
+		end
+	end
 
-  return nil
+	return nil
 end
 
 local function do_paste(node, nodes, action_type, action_fn)
-  node = lib.get_last_group_node(node)
-  if node.name == ".." then
-    return
-  end
+	node = lib.get_last_group_node(node)
+	if node.name == ".." then
+		return
+	end
 
-  local destination = node.absolute_path
-  local stats = luv.fs_stat(destination)
-  local is_dir = stats and stats.type == "directory"
+	local destination = node.absolute_path
+	local stats = luv.fs_stat(destination)
+	local is_dir = stats and stats.type == "directory"
 
-  if not is_dir then
-    destination = vim.fn.fnamemodify(destination, ":p:h")
-  elseif not node.open then
-    destination = vim.fn.fnamemodify(destination, ":p:h:h")
-  end
+	if not is_dir then
+		destination = vim.fn.fnamemodify(destination, ":p:h")
+	elseif not node.open then
+		destination = vim.fn.fnamemodify(destination, ":p:h:h")
+	end
 
-  local dest_paths = {}
-  for _, entry in ipairs(nodes) do
-    local dest = utils.path_join { destination, entry.name }
-    dest = do_single_paste(entry.absolute_path, dest, action_type, action_fn)
-    if dest ~= nil then
-      table.insert(dest_paths, dest)
-    end
-  end
+	local dest_paths = {}
+	for _, entry in ipairs(nodes) do
+		local dest = utils.path_join({ destination, entry.name })
+		dest = do_single_paste(entry.absolute_path, dest, action_type, action_fn)
+		if dest ~= nil then
+			table.insert(dest_paths, dest)
+		end
+	end
 
-  return dest_paths
+	return dest_paths
 end
 
 function M.delete(nodes)
-  -- TODO: unify_ancestors
-  for _, node in ipairs(nodes) do
-    remove(node)
-  end
+	-- TODO: unify_ancestors
+	for _, node in ipairs(nodes) do
+		remove(node)
+	end
 end
 
 function M.copy(node, nodes)
-  -- TODO: unify_ancestors
-  return do_paste(node, nodes, "copy", do_copy)
+	-- TODO: unify_ancestors
+	return do_paste(node, nodes, "copy", do_copy)
 end
 
 function M.cut(node, nodes)
-  -- TODO: unify_ancestors
-  return do_paste(node, nodes, "copy", do_cut)
+	-- TODO: unify_ancestors
+	return do_paste(node, nodes, "copy", do_cut)
 end
 
 return M
